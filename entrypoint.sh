2d238e8003141c9f396f5603b0242ef9f20e0d0a
lueyoung <lueyoung7@gmail.com>
2022-11-23 10:58:30 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python3 /src/messager.py --enable "${INPUT_ENABLE}" \
+python3 /src/messager.py \
+  --enable "${INPUT_ENABLE}" \
   --pushkey "${INPUT_PUSHKEY}" \
   --text "${INPUT_TEXT}"
